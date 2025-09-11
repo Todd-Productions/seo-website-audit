a0c95523723de47b119faee5a3ee1675db73719b
Andrew Stanton <andrew@toddprod.com>
2025-09-11 14:47:49 -0400

    Basic crawling setup

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,36 @@ import { Dataset, PlaywrightCrawler } from "crawlee";
     maxConcurrency: 10,
 
     // Processes each of the crawled pages
-    async requestHandler({ request, page, enqueueLinks, log }) {
+    async requestHandler({ request, page, enqueueLinks }) {
       const title = await page.title();
-      log.info(`Title of ${request.loadedUrl}: ${title}`);
 
       // Save results as JSON to ./storage/datasets/default
-      await Dataset.pushData({ title, url: request.loadedUrl });
+      await Dataset.pushData({
+        title,
+        url: request.loadedUrl,
+        headers: request.headers,
+      });
 
       // Extract links from the current page
       // and add them to the crawling queue
       await enqueueLinks();
     },
+
+    // Failed Request Handler
+    async failedRequestHandler({ request, error }) {
+      await Dataset.pushData({
+        url: request.loadedUrl,
+        error,
+      });
+    },
   });
 
   // Add URL to crawl
-  await crawler.run(["https://www.stantoncreativemedia.com"]);
+  await crawler.run(["https://www.toddproductions.com"]);
+
+  // Accessing the dataset
+  const data = await Dataset.getData();
+  data.items.forEach((item) => {
+    console.log({ item });
+  });
 })();
